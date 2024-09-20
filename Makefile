033cf96a3979275b700a96fa15ac65eb33ceae5a
Jorge Rafael <jorge.rafael@externos-cl.cencosud.com>
2024-09-20 19:25:23 -0300

    feat(app): call send many on anvil node

--- Makefile
+++ Makefile
@@ -10,6 +10,7 @@ CONTRACTS_PATH=./contracts
 BINDINGS_FOLDER=bindings
 BINDINGS_CRATES_FOLDER=$(CRATES_FOLDER)/$(BINDINGS_FOLDER)
 BINDINGS_OUT_PATH=$(CONTRACTS_PATH)/out/$(BINDINGS_FOLDER)
+MAIN_CONTRACT=src/MultiSend.sol:MultiSend
 
 # Target for generating bindings
 bindings:
@@ -52,3 +53,19 @@ setup:
 
 # Declare phony targets
 .PHONY: build build-release clean fmt bindings
+
+
+run-local-node:
+	anvil --block-time 10
+
+verify-contract:
+	forge verify-contract 0xdfbfbdfb29de74e0a3a588abe4be512e03a54744 —verifier=blockscout --verifier-url=https://eth-sepolia.blockscout.com/api/
+
+deploy:
+	forge create --root $(CONTRACTS_PATH) --rpc-url http://127.0.0.1:8545 --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80 $(MAIN_CONTRACT)
+
+ethernal:
+	npx --yes ethernal listen
+
+advisories:
+	cargo deny -c always --locked -L info check advisories
--- app/src/main.rs
+++ app/src/main.rs
@@ -1,19 +1,28 @@
-use alloy::{
-    primitives::Address,
-    providers::{builder, Provider},
-};
+use bindings::multi_send::MultiSend;
+
+//use ethers::{providers::test_provider::SEPOLIA, types::{Address, U256}};
+use ethers::prelude::*; 
+
 use eyre::Result;
-use foundry_contracts::counter::Counter;
+use std::sync::Arc;
+
+const RPC_URL: &str = "http://localhost:8545";
+const SIGNING_KEY: &str = "ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let provider = builder().with_recommended_fillers().on_anvil_with_wallet();
+    let provider = Provider::<Http>::try_from(RPC_URL)?; //SEPOLIA.provider();
+    let signer = SIGNING_KEY.parse::<LocalWallet>()?;
+    let provider = provider.with_signer(signer);
+    let provider = Arc::new(provider);
 
-    let address = "0x0000000000000000000000000000000000000000".parse::<Address>()?;
-
-    let _contract = Counter::new(address, provider.clone());
-
-    let blk = provider.get_block_number().await?;
-    println!("Hello, world! {}", blk);
+    let address = "0x5FbDB2315678afecb367f032d93F642f64180aa3".parse::<Address>()?;
+    let to = vec!["0x70997970C51812dc3A010C7d01b50e0d17dc79C8".parse::<Address>()?, 
+            "0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC".parse::<Address>()?];
+    let value = vec!["10000".parse::<U256>()?, "20000".parse::<U256>()?];
+    let multi_send = MultiSend::new(address, provider);
+    multi_send.send_many(to, value).await?;
+    println!("Send done!");
+    
     Ok(())
 }
