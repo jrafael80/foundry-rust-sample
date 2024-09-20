a21f4bef0378875e03f8f8c511c181f267ca7c22
Jorge Rafael <jorge.rafael@externos-cl.cencosud.com>
2024-09-20 19:21:39 -0300

    fix: reduce imported contracts

--- contracts/src/MultiSend.sol
+++ contracts/src/MultiSend.sol
@@ -2,9 +2,8 @@
 
 pragma solidity >=0.8.0 <0.9.0;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 contract MultiSend {
     using SafeERC20 for IERC20;
